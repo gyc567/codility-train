665755674092e8b209793fea0b9d21ac296f6d48
eric567 <gyc567@126.com>
2016-11-09 11:09:34 +0800

    update the fibonacci solution

--- src/main/java/com/github/ericguo/Fibonacci.java
+++ src/main/java/com/github/ericguo/Fibonacci.java
@@ -1,6 +1,7 @@
 package com.github.ericguo;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 
 /**
  * Created by eric567 [email:gyc567@126.com]
@@ -8,12 +9,66 @@ import java.math.BigInteger;
  */
 public class Fibonacci {
 
+    private static HashMap<Object, Long> fiboMap=new HashMap<>();
+
     public static final int calculate(int n) {
 
         if (n <= 1) return n;
         else return calculate(n - 1) + calculate(n - 2);
+
     }
 
+    public static final long calculateFib(long n) {
+
+        if (n <= 1) return n;
+        if (n == 2) return 1;
+        long[] fib = new long[(int)n+1];
+        for (long i = 0; i <=n; i++) {
+            if (i<= 1) {
+                fib[(int)i] = i;
+            } else if (i == 2) {
+                fib[(int)i]=1;
+            } else {
+                fib[(int)i] = fib[(int)i - 1] + fib[(int)i - 2];
+            }
+
+        }
+
+
+        return fib[(int)n];
+
+    }
+
+
+    private static long[] cache=new long[100];
+    public static long fibonacciWithStaticArray(long k) {
+        if (k == 0 || k == 1) {
+            return k;
+        }
+        if(cache[(int)k]!=0)return cache[(int)k];
+
+        else
+            return fibonacciWithStaticArray(k-1)+fibonacciWithStaticArray(k-2);
+
+
+    }
+    public static long fibonacciWithMap(long k) {
+        if (k == 0 || k == 1) {
+            return k;
+        }
+
+        try {
+            // do not calculate anymore, just return it
+            return fiboMap.get(k);
+        } catch (NullPointerException np) {
+            // has to be calculated once, then save it
+            // calculation part
+            long value = fibonacciWithMap(k - 1) + fibonacciWithMap(k - 2);
+            // saving part
+            fiboMap.put(k, value);
+            return value;
+        }
+    }
     public static BigInteger fibonacci(int n) {
 
 
--- src/test/java/com/github/ericguo/FibonacciTest.groovy
+++ src/test/java/com/github/ericguo/FibonacciTest.groovy
@@ -10,27 +10,92 @@ class FibonacciTest extends Specification {
     def "Calculate,give:#n fibonacci number shall be #f"() {
 
         expect:
-        f==Fibonacci.calculate(n)
+        f == Fibonacci.calculate(n)
         where:
-        f|n
-        0|0
-        1|1
-        1|2
-        2|3
-        3|4
+        f            | n
+        0            | 0
+        1            | 1
+        1            | 2
+        2            | 3
+        3            | 4
+        5            | 5
+        8            | 6
+        5702887  | 34
+        63245986 | 39
+        956722026041 | 59
     }
 
 
-    def "give:#v the fibonacci number shall be #f"()
-    {
+    def "give:#v the fibonacci number shall be #f"() {
         expect:
-        f==Fibonacci.fibonacci(v)
+        f == Fibonacci.fibonacci(v)
         where:
-        f|v
-        0|0
-        1|1
-        1|2
-        2|3
-        3|4
+        f            | v
+        0            | 0
+        1            | 1
+        1            | 2
+        2            | 3
+        3            | 4
+        5            | 5
+        8            | 6
+        5702887  | 34
+        63245986 | 39
+        956722026041 | 59
+    }
+
+    def "give:#v the calculateFib number shall be #f"() {
+        expect:
+        f == Fibonacci.calculateFib(v)
+        where:
+        f        | v
+        0        | 0
+        1        | 1
+        1        | 2
+        2        | 3
+        3        | 4
+        5        | 5
+
+        8        | 6
+        5702887  | 34
+        63245986 | 39
+
+    }
+
+    def "give:#v the fibonacciWithMap number shall be #f"() {
+        expect:
+        f == Fibonacci.fibonacciWithMap(v)
+        where:
+        f        | v
+        0        | 0
+        1        | 1
+        1        | 2
+        2        | 3
+        3        | 4
+        5        | 5
+
+        8        | 6
+        5702887  | 34
+        63245986 | 39
+        956722026041 | 59
+
+    }
+
+    def "give:#v the fibonacciWithStaticArray number shall be #f"() {
+        expect:
+        f == Fibonacci.fibonacciWithStaticArray(v)
+        where:
+        f        | v
+        0        | 0
+        1        | 1
+        1        | 2
+        2        | 3
+        3        | 4
+        5        | 5
+
+        8        | 6
+        5702887  | 34
+        63245986 | 39
+        //956722026041 | 59
+
     }
 }
